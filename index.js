cc7b0eee89a3132e7af8c2bdffe40ccdaf399fc7
miroslavpojer <miroslav.pojer@absa.africa>
2024-05-15 15:02:59 +0200

    - Improving amount of log information when error some.

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ async function run() {
         // Running the Python script with an input
         exec(command, (error, stdout, stderr) => {
             if (error) {
+                console.error(`Error executing script: ${error.message}`);
+                console.error(`Error code: ${error.code}`);
+                console.error(`Error signal: ${error.signal}`);
+                console.error(`Error stack: ${error.stack}`);
                 core.setFailed(`Execution error: ${error}`);
                 return;
             }
