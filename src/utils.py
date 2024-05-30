371372f8427b63809d84c5e227d044e6296e86b9
MobiTikula <tobias.mikula@absa.africa>
2024-05-30 13:17:32 +0200

    Utils script folder refactored.

--- src/utils.py
+++ src/utils.py
@@ -7,32 +7,8 @@ These functions can be imported and used in other src as needed.
 
 import os
 import json
-import argparse
 import requests
-from typing import Union, Dict
-
-
-def parse_arguments(description: str) -> argparse.Namespace:
-    """
-        Parses the command line arguments provided by the user when running the script.
-        As default, the script will use the `config.json` file for configuration.
-
-        @param description: The description of the script.
-
-        @return: The parsed command line arguments.
-    """
-    parser = argparse.ArgumentParser(
-        description=description)
-
-    parser.add_argument('-c', '--config',
-                        type=str,
-                        required=False,
-                        default='config.json',
-                        help='Configuration JSON file with parameters for data mining from GitHub. Default: config.json')
-
-    args = parser.parse_args()
-
-    return args
+from typing import Union
 
 
 def initialize_request_session(github_token: str) -> requests.sessions.Session:
@@ -74,14 +50,15 @@ def ensure_folder_exists(folder_name: str, current_dir: str) -> None:
 
 
 def save_state_to_json_file(state_to_save: Union[list, dict], object_type: str, output_directory: str, state_name: str) -> str:
-    """Saves a list state to a JSON file.
+    """
+    Saves a list state to a JSON file.
 
-        @param state_to_save: The list or dict to be saved.
-        @param object_type: The object type of the state (e.g., 'feature', 'project').
-        @param output_directory: The directory, where the file will be saved.
-        @param state_name: The naming of the state.
+    @param state_to_save: The list or dict to be saved.
+    @param object_type: The object type of the state (e.g., 'feature', 'project').
+    @param output_directory: The directory, where the file will be saved.
+    @param state_name: The naming of the state.
 
-        @return: The name of the output file.
+    @return: The name of the output file.
     """
     # Prepare the unique saving naming
     sanitized_name = state_name.lower().replace(" ", "_").replace("-", "_")
